09c89b3eb9122b36169f301e05bee9bcd939534f
Clement <clement.dumas@ens-paris-saclay.fr>
2024-08-12 13:34:49 +0200

    fixed script

--- compute_results.sh
+++ compute_results.sh
@@ -1,9 +1,8 @@
 # Plot 1
-# python run_notebook.py -n patchscope_shifted_translation -b 8 -m Llama-2-7b\
-#  --model-path /dlabscratch1/public/llm_weights/llama2_hf/Llama-2-7b-hf
+python run_notebook.py -n patchscope_shifted_translation -b 8 -m meta-llama/Llama-2-7b-hf
 
 # Plot 2
-python run_notebook.py -n obj_patch_translation -b 8 -m Llama-2-7b --model-path /dlabscratch1/public/llm_weights/llama2_hf/Llama-2-7b-hf
+python run_notebook.py -n obj_patch_translation -b 8 -m meta-llama/Llama-2-7b-hf 
 
 # Patchscope lens
-python run_notebook.py -n translation_lens -b 8 -m Llama-2-7b --model-path /dlabscratch1/public/llm_weights/llama2_hf/Llama-2-7b-hf
+python run_notebook.py -n translation_lens -b 8 -m meta-llama/Llama-2-7b-hf
